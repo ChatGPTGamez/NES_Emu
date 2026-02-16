ee92b7281fadee903a38ccf470fbb80437aecd02
Jon Limle <gitchatgpt9@gmail.com>
2026-02-16 17:43:21 -0500

    Update 3 - Graphics rendered with gameplay 3

--- src/nes/mapper.c
+++ src/nes/mapper.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 Mapper* MapperNROM_Create(Cart* cart);  // implemented in mapper_nrom.c
+Mapper* MapperMMC1_Create(Cart* cart);  // implemented in mapper_mmc1.c
 Mapper* MapperUxROM_Create(Cart* cart); // implemented in mapper_uxrom.c
 
 Mapper* Mapper_Create(Cart* cart, u32 mapper_id)
@@ -12,6 +13,7 @@ Mapper* Mapper_Create(Cart* cart, u32 mapper_id)
 
     switch (mapper_id) {
         case 0: return MapperNROM_Create(cart);
+        case 1: return MapperMMC1_Create(cart);
         case 2: return MapperUxROM_Create(cart);
         default:
             NES_LOGE("Mapper_Create: unsupported mapper %u", mapper_id);
